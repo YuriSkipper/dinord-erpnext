b1b3d9f33312522dd4d82b7c3f2f912b882c89a1
ykovalenkoiconsoft <ykovalenko@iconsoft.ru>
2023-04-20 04:13:45 +0300

    ERPNext custom fields transport between different ERPNext instances

--- dinord_erpnext_customization/hooks.py
+++ dinord_erpnext_customization/hooks.py
@@ -202,4 +202,6 @@ app_license = "MIT"
 
 # Fixtures - ERPNext customization transport between different ERPNext instances
 
-fixtures = ['Report', 'Role Profile', 'Role', 'DocPerm', 'Custom Field', 'Custom Role', 'Custom DocPerm', 'Notification Settings', 'Server Script', 'Property Setter', 'Workflow', 'Workflow State', 'Workflow Action']
+fixtures = [
+    "Custom Field"
+]
