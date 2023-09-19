134ca162c950b892c56e84f4cce3792cacf3a348
yurykovalenko-git <yury.kovalenko@bytelabs.ru>
2023-09-19 00:01:33 +0000

    custom fields correction

---
+++ dinord_erpnext_customization/dinord_erpnext_customization/doctype/identification_table/test_identification_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Dinord Yury Kovalenko and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestIdentificationTable(FrappeTestCase):
+	pass
